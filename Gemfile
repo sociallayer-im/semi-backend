4715a2f147ea194bb01ca24346bdb003be32c340
jiangplus <jiang.plus.times@gmail.com>
2025-05-15 14:42:12 +0800

    add dotenv-rails

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source "https://rubygems.org"
 
+gem "dotenv-rails"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 8.0.2"
 # Use postgresql as the database for Active Record
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,27 @@ class HomeController < ApplicationController
   def index
     render json: { message: "Hello, World!" }
   end
+
+  def send_sms
+    client = RPCClient.new(
+      access_key_id: ENV["ACCESS_KEY_ID"],
+      access_key_secret: ENV["ACCESS_KEY_SECRET"],
+      endpoint: "https://dysmsapi.aliyuncs.com",
+      api_version: "2017-05-25"
+    )
+
+    response = client.request(
+      action: "SendSms",
+      params: {
+        "SignName": "小海星平台",
+        "TemplateCode": "SMS_262555238",
+        "PhoneNumbers": "#{phone}",
+        "TemplateParam": "{\"code\":\"#{code}\"}"
+      },
+      opts: {
+        method: "POST",
+        format_params: true
+      }
+    )
+  end
 end
